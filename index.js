fee230ff1956388b0fdd5aec82d57fb207f067f3
Jyun <taiwanman1212@gmail.com>
2023-07-11 13:53:55 +0800

    init3

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// 设置静态文件目录
+app.use(express.static('public'));
+
+// 根路由的处理程序
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// 启动服务器
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
