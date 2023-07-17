69bbcfddcc810b71c818ef2aa5124c1a7ab8ab62
Jyun <taiwanman1212@gmail.com>
2023-07-17 15:55:52 +0800

    final stage

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 const d3 = import('d3');
 
-// 设置静态文件目录
+// 設置靜態文件目錄
 app.use(express.static('public'));
 
-// 根路由的处理程序
+// 其他路由和中間件
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-// 启动服务器
+// 設置靜態文件目錄
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
