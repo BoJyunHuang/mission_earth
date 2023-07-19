adcaaf91f40300278fd0797a9337affb66f3828a
BoJyunHuang <taiwanman1212@gmail.com>
2023-07-19 14:28:50 +0800

    fix problem

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const { spawn } = require('child_process');
 const app = express();
-const d3 = import('d3');
 
 // 設置靜態文件目錄
 app.use(express.static('public'));
@@ -10,7 +10,20 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+// 呼叫並執行py檔
+app.get('/execute_script', (req, res) => {
+    const scriptPath = 'C:\\Users\\taiwa\\OneDrive\\桌面\\test\\python-map\\weather.py';
+    const pythonProcess = spawn('python', [scriptPath]);
+    pythonProcess.on('close', (code) => {
+        if (code === 0) {
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(500);
+        }
+    });
+});
+
 // 設置靜態文件目錄
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(8080, () => {
+    console.log('Server is running on http://localhost:8080');
 });
